209e045fd1630a69ad93fea97dfbcda3b52c447b
Tsiky <tsiky.andriam@gmail.com>
2017-11-16 23:37:34 +0100

    FEATURE: Add params for action multimedias:text

--- src/components/ActionsCreateView.vue
+++ src/components/ActionsCreateView.vue
@@ -23,20 +23,35 @@
           </option>
         </select>
       </div>
+      <div v-if="typeKey === 0 && nameKey === 0">
+        <div class="ui divider"></div>
+        <div class='field'>
+          <label>Text</label>
+          <input v-model='textValue' type='text'>
+        </div>
+        <div class='field'>
+          <label>Background-color</label>
+          <input v-model='backgroundColorValue' type='text'>
+        </div>
+        <div class='field'>
+          <label>Vibration</label>
+          <input v-model='vibrationValue' type='number' step='0.1'>
+        </div>
+      </div>
     </div>
 
-    <router-link to='/actions/'>
-      <button class='ui labeled icon button'>
-        <i class='chevron left icon'></i>
-        Back
+    <div class="actions-create-buttons">
+      <router-link to='/actions/'>
+        <button class='ui labeled icon button'>
+          <i class='chevron left icon'></i>
+          Back
+        </button>
+      </router-link>
+      <button v-on:click='addAction()' v-bind:class="{ loading: loading }" class='ui primary labeled icon button'>
+        <i class='checkmark icon'></i>
+        Create
       </button>
-    </router-link>
-    <!--<router-link to='/actions/'>-->
-    <button v-on:click='addAction()' class='ui primary labeled icon button'>
-      <i class='checkmark icon'></i>
-      Create
-    </button>
-    <!--</router-link>-->
+    </div>
   </div>
 </template>
 
@@ -49,18 +64,22 @@
     data () {
       return {
         'labelValue': '',
-        'typeKey': '0',
+        'textValue': '',
+        'backgroundColorValue': '',
+        'vibrationValue': '',
+        'typeKey': 0,
         'types': ['multimedia', 'quiz', 'color', 'hardware', 'webview', 'arduino', 'scene control', 'update'],
-        'nameKey': '0',
+        'nameKey': 0,
         'names': [
-          ['video', 'streaming', 'audio', 'image', 'text'],
+          ['text', 'video', 'streaming', 'audio', 'image'],
           ['question', 'answer'],
           ['color'],
           ['torch'],
           ['url'],
           ['HIGH', 'LOW'],
           ['scene_activate', 'scene_sendkeystroke'],
-          ['medias', 'gps']]
+          ['medias', 'gps']],
+        'loading': false
       }
     },
     mounted: function () {
@@ -74,28 +93,32 @@
     },
     methods: {
       addAction: function () {
-        let newAction = {
-          'name': this.labelValue,
-          'module': this.types[this.typeKey],
-          'value': this.names[this.typeKey][this.nameKey],
-          'data': {
-            'file': 'Hello word !',
-            'default-sound': 'sound.mp3',
-            'vibration': 0.5,
-            'background-color': '#020202'
-          },
-          'aref': 'string',
-          'situation': 'situationTest'
-        }
+        if (this.labelValue !== '') {
+          this.loading = true
+          let newAction = {
+            'name': this.labelValue,
+            'module': this.types[this.typeKey],
+            'value': this.names[this.typeKey][this.nameKey],
+            'data': {
+              'file': this.textValue,
+              'vibration': this.vibrationValue !== '' ? this.vibrationValue : 0,
+              'background-color': this.backgroundColorValue
+            },
+            'aref': 'string',
+            'situation': 'situationTest'
+          }
 
-        this.$http.put('/api/myapp/action', newAction).then(response => {
-          console.log(response.body)
-          Store.commit('actions/addAction', newAction)
-          Router.push({ path: '/actions' })
-        }, response => {
-          // error callback
-          console.log(response.body)
-        })
+          this.$http.put('/api/myapp/action', newAction).then(response => {
+            console.log(response.body)
+            Store.commit('actions/addAction', newAction)
+            this.loading = false
+            Router.push({ path: '/actions' })
+          }, response => {
+            // error callback
+            console.log(response.body)
+            this.loading = false
+          })
+        }
       }
     }
   }
@@ -103,5 +126,7 @@
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
-
+  .actions-create-buttons {
+    margin-top: 1.5rem;
+  }
 </style>
